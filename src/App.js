03e4532fe3c07b01c9e28e030e4ba7e0b9171216
josephhart-droid <56367446+josephhart-droid@users.noreply.github.com>
2026-01-30 11:53:18 +0000

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>BIG ROCKS</h1>
+      <p>A minimalist, zero bloat roadmapping tool to effectively prioritize your ideas.</p>
+      {/* Add your roadmap components here */}
+    </div>
+  );
+}
+
+export default App;
