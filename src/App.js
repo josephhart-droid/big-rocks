d12bb4c038726746e85b35e1e66fe9e4fdaa5f16
josephhart-droid <56367446+josephhart-droid@users.noreply.github.com>
2026-02-02 22:48:16 +0000

    Update App.js
    noise up

--- src/App.js
+++ src/App.js
@@ -545,7 +545,7 @@ export default function App() {
         margin: 0,
         padding: 0,
         backgroundColor: '#F0F0F0',
-        backgroundImage: `url("data:image/svg+xml,%3Csvg viewBox='0 0 400 400' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.9' numOctaves='4' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)' opacity='0.12'/%3E%3C/svg%3E")`,
+        backgroundImage: `url("data:image/svg+xml,%3Csvg viewBox='0 0 400 400' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.9' numOctaves='4' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)' opacity='0.3'/%3E%3C/svg%3E")`,
         fontFamily: '"Work Sans", sans-serif',
         overflowX: 'hidden',
       }}>
