009c2a2f24d0346e7d25e428253102d00a6302d4
josephhart-droid <56367446+josephhart-droid@users.noreply.github.com>
2026-02-03 12:34:31 +0000

    Update App.js
    semantic fix

--- src/App.js
+++ src/App.js
@@ -1188,6 +1188,7 @@ export default function App() {
                             }} />
                           )}
                         </>
+                        </>
                       )}
                     </div>
                   </div>
